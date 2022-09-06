979115d63550d5858733bd27d8183691516c0b37
Jake Leogabrel Galindo <jake@rightchoice.email>
2022-09-06 16:58:33 +0800

    rsbsa update tuesday 15

--- index.php
+++ index.php
@@ -108,17 +108,17 @@ if(isset($_POST["btnsubmit"])) {
                     <img src="images/Icons/logo_doa.png" class="w-64" alt="Agriculture">
                     <h1 class="text-3xl text-center text-white">Department of Agriculture in Candelaria, Quezon Province</h1>
                     <div class="flex flex-col">
-                        <div class="flex flex-col gap-2">
-                            <h2>Mandate</h2>
+                        <div class="flex flex-col gap-2 items-center text-white">
+                            <h2 class="font-bold">Mandate</h2>
                             <p>The Office of the Municipal Agriculture is an agency of the Philippine government responsible for the promotion of the Agriculture & Fisheries development and growth. In partnership with the Department of Agriculture, provide benefits of development to the poor, especially in the rural areas</p>
                         </div>
                         <div class="flex">
-                            <div class="flex flex-col gap-2">
-                                <h2>Vision</h2>
+                            <div class="flex flex-col gap-2 w-1/2 text-white">
+                                <h2 class="font-bold">Vision</h2>
                                 <p>Prosperous rural communities built on profitable farms that provide surplus for agro-industry and guarantees food security</p>
                             </div>
-                            <div class="flex flex-col gap-2">
-                                <h2>Mission</h2>
+                            <div class="flex flex-col gap-2 w-1/2 text-white">
+                                <h2 class="font-bold">Mission</h2>
                                 <p>Increase income of farmers & fisherfolks, thereby contributing to the achievement of national goal of alleviating poverty, generating productive opportunities, fostering social justice and equity, and promoting sustainable economic growth</p>
                             </div>
                         </div>
