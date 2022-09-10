1b2aaf0e386dd1bf20052e826f40b02e2f6d7923
Jake Leogabrel Galindo <jake@rightchoice.email>
2022-09-10 20:58:55 +0800

    rsbsa update sat 18

--- intervention-table.php
+++ intervention-table.php
@@ -86,7 +86,7 @@ if (!isset($_SESSION['loggedin'])) {
                             <th>Crop</th>
                             <th>Land Size</th>
                             <th>Status</th>
-                            <th class="super-hide" style="display:hidden;">Action</th>
+                           
                         </tr>
                     </thead>
                     <tbody>
