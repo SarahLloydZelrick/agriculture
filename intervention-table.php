18cd99227358180ea0036eb5e70fb050ea2219c3
Jake Leogabrel Galindo <jake@rightchoice.email>
2022-09-11 23:36:46 +0800

    rsbsa update sund 21

--- intervention-table.php
+++ intervention-table.php
@@ -53,7 +53,7 @@ if (!isset($_SESSION['loggedin'])) {
         
     </script>
     <script>
-        var brgy = $('#release_barangay').val($(this).find('option:selected').val());
+        var brgy = $( "#release_barangay option:selected" ).val();
         function fetch_land(val)
         {
             $.ajax({
