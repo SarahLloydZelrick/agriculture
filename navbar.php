f1f2c16095cf912fb44e67dfe9812d88545b9d00
Jake Leogabrel Galindo <jake@rightchoice.email>
2022-09-11 10:44:00 +0800

    rsbsa update sund 2

--- navbar.php
+++ navbar.php
@@ -109,6 +109,11 @@
         <span>CROP ANALYSIS</span>
       </a>
     </li>
+    <li class="relative farmer" style="display:none;">
+      <a class="flex items-center text-sm py-4 px-6 h-12 overflow-hidden text-white text-ellipsis whitespace-nowrap rounded hover:text-blue-600 hover:bg-blue-50 transition duration-300 ease-in-out" href="cropanalysis.php" data-mdb-ripple="true" data-mdb-ripple-color="primary">
+        <span>FARMER INTERVENTION</span>
+      </a>
+    </li>
   </ul>
     </div>
 
