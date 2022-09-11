d806715c9efb891be3fa7f112a05a991e5371b0d
Jake Leogabrel Galindo <jake@rightchoice.email>
2022-09-11 23:22:10 +0800

    rsbsa update sund 19

--- fetch_land.php
+++ fetch_land.php
@@ -5,7 +5,7 @@ if(isset($_POST['get_commodity']))
 
     $commodity = $_POST['get_commodity'];
     $barangay = $_POST['get_barangay'];
-    $sqlbrgy3 = "SELECT DISTINCT `size` FROM `tbl_intervention` WHERE `crop` = '$commodity'";
+    $sqlbrgy3 = "SELECT DISTINCT `size` FROM `tbl_intervention` WHERE `farmbarangay` = '$barangay' AND `crop` = '$commodity'";
     $resultbrgy3 = mysqli_query($con, $sqlbrgy3);
         if (mysqli_num_rows($resultbrgy3) > 0) {
             while($row = mysqli_fetch_assoc($resultbrgy3)) {
