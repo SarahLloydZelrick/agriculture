e14a8edf0c30d84ea626a0fc5996f05bebcb7683
Jake Leogabrel Galindo <jake@rightchoice.email>
2022-09-11 22:09:39 +0800

    rsbsa update sund 9

--- fetch_crops.php
+++ fetch_crops.php
@@ -1,14 +1,17 @@
 <?php
 if(isset($_POST['get_option']))
 {
-include "config.php";
+    include "config.php";
 
- $barangay = $_POST['get_option'];
- $find=mysql_query("SELECT DISTINCT `crop` FROM `tbl_intervention` WHERE `farmbarangay` = '$barangay'");
- while($row=mysql_fetch_array($find))
- {
-  echo "<option>".$row['crop']."</option>";
- }
- exit;
+    $barangay = $_POST['get_option'];
+    $sqlbrgy2 = "SELECT DISTINCT `crop` FROM `tbl_intervention` WHERE `farmbarangay` = '$barangay'";
+    $resultbrgy2 = mysqli_query($con, $sqlbrgy2);
+        if (mysqli_num_rows($resultbrgy2) > 0) {
+            while($row = mysqli_fetch_assoc($resultbrgy2)) {
+                echo "<option>".$row['crop']."</option>";
+            }
+        }
 }
 ?>
+
+
