294c7d6c06bf05c31159095296c8249f1dda4cac
Jake Leogabrel Galindo <jake@rightchoice.email>
2022-09-13 22:15:19 +0800

    rsbsa update tues 15

--- intervention-table.php
+++ intervention-table.php
@@ -105,7 +105,7 @@ if (!isset($_SESSION['loggedin'])) {
                     Deleted
                 </button>
                 <button 
-                class="inline-block px-6 py-2.5 bg-yellow-500 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-yellow-600 hover:shadow-lg focus:bg-yellow-600 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-yellow-700 active:shadow-lg transition duration-150 ease-in-out"
+                class="inline-block px-6 py-2.5 bg-red-500 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-red-600 hover:shadow-lg focus:bg-red-600 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-red-700 active:shadow-lg transition duration-150 ease-in-out"
                 onclick="viewReleased()"
                 >
                     Released
@@ -836,6 +836,18 @@ if (!isset($_SESSION['loggedin'])) {
         } );
         $(document).ready( function () {
             $('#notreceived_table').DataTable(
+                {
+                    dom: 'Bfrtip',
+                    buttons: [
+                        'csv', 'excel', 'pdf', 'print'
+                    ],
+                    responsive: true,
+                    "pageLength": 6
+                }
+            );
+        } );
+        $(document).ready( function () {
+            $('#released_table').DataTable(
                 {
                     dom: 'Bfrtip',
                     buttons: [
