30f97e9b2c694d61258345093b541bbc06734c0a
Jake Leogabrel Galindo <jake@rightchoice.email>
2022-10-17 19:09:10 +0800

    rsbsa update mond

--- about.php
+++ about.php
@@ -95,6 +95,18 @@
                     </div>
                 </div>
             </div>
+            <div class="flex justify-center">
+                <div class="rounded-lg shadow-lg bg-white max-w-sm">
+                    <a href="#!">
+                    <img class="rounded-t-lg" src="https://mdbootstrap.com/img/new/standard/nature/184.jpg" alt=""/>
+                    </a>
+                    <div class="p-6">
+                    <h5 class="text-gray-900 text-xl font-medium mb-2">
+                        Seeds, Planting Materials and Fertilizers Subsidy and Distribution
+                    </h5>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
     <footer class="text-center text-white" style="background-color: #f1f1f1;">
