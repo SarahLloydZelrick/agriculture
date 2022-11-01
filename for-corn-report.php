a23fd7bdd2a60a9b40420befb4045de87b999f6b
Jake Leogabrel Galindo <jake@rightchoice.email>
2022-11-01 19:26:47 +0800

    rsbsa update

--- for-corn-report.php
+++ for-corn-report.php
@@ -32,6 +32,65 @@ if (!isset($_SESSION['loggedin'])) {
         include "navbar.php";
         include "topbar.php";
 
+        $reporttype = "CornPlanting";
+        $todate = $_GET['dateto'];
+        $fromdate = $_GET['datefrom'];
+
+        if (isset($_POST['btnsave'] )) {
+            $geocode = $_POST['geocode'];
+            $barangay1 = $_POST['barangay1'];
+            if($_POST['nooffarmer1'] = ""){
+                $nooffarmer1 = 0;
+            }else{
+                $nooffarmer1 = $_POST['nooffarmer1'];
+            }
+            if($_POST['hybridyellow1'] = ""){
+                $hybridyellow1 = 0;
+            }else{
+                $hybridyellow1 = $_POST['hybridyellow1'];
+            }
+            if($_POST['hybridwhite1'] = ""){
+                $hybridwhite1 = 0;
+            }else{
+                $hybridwhite1 = $_POST['hybridwhite1'];
+            }
+            if($_POST['hybridtotal1'] = ""){
+                $hybridtotal1 = 0;
+            }else{
+                $hybridtotal1 = $_POST['hybridtotal1'];
+            }
+            if($_POST['opvyellow1'] = ""){
+                $opvyellow1 = 0;
+            }else{
+                $opvyellow1 = $_POST['opvyellow1'];
+            }
+            if($_POST['opvwhite1'] = ""){
+                $opvwhite1 = 0;
+            }else{
+                $opvwhite1 = $_POST['opvwhite1'];
+            }
+            if($_POST['opvtotal1'] = ""){
+                $opvtotal1 = 0;
+            }else{
+                $opvtotal1 = $_POST['opvtotal1'];
+            }
+            if($_POST['grandyellow1'] = ""){
+                $grandyellow1 = 0;
+            }else{
+                $grandyellow1 = $_POST['grandyellow1'];
+            }
+            if($_POST['grandwhite1'] = ""){
+                $grandwhite1 = 0;
+            }else{
+                $grandwhite1 = $_POST['grandwhite1'];
+            }
+            if($_POST['grandtotal1'] = ""){
+                $grandtotal1 = 0;
+            }else{
+                $grandtotal1 = $_POST['grandtotal1'];
+            }
+        }
+
     ?>
     <div class="container flex flex-col p-10 ml-0 w-fit md:ml-60 " >
         <h2 class="text-2xl font-bold">Consolidation Corn Planting</h2>
@@ -47,7 +106,7 @@ if (!isset($_SESSION['loggedin'])) {
                     </tr>
                     <tr>
                         <td rowspan="1" colspan="3">Hybrid</td>
-                        <td rowspan="1" colspan="3">OPW</td>
+                        <td rowspan="1" colspan="3">OPV</td>
                         <td rowspan="1" colspan="3">Grand Total</td>
                         
                     </tr>
@@ -126,7 +185,7 @@ if (!isset($_SESSION['loggedin'])) {
                         </td>
                     </tr>
                 </table>
-                <input type="submit" value="Save" class="btn-primary">
+                <input type="submit" value="Save" name="btnsave" class="btn-primary-s">
             </form>
         </div>
 
