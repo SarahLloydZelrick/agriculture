a5440461be7358f436f85ba3b470734c04a10223
Jake Leogabrel Galindo <jake@rightchoice.email>
2022-11-01 15:38:09 +0800

    rsbsa update

--- corn-report.php
+++ corn-report.php
@@ -28,7 +28,7 @@ if (!isset($_SESSION['loggedin'])) {
     <div class="container flex flex-col p-10 ml-0 w-fit md:ml-60 " >
         <h2 class="text-2xl font-bold">Consolidation Corn Planting</h2>
         <div class="flex flex-col gap-2">
-            <form action="" class="flex flex-col gap-2">
+            <form action="for-corn-report.php" class="flex flex-col gap-2">
                 <div class="flex flex-row">
                     <div class="flex flex-col">
                         <label>Date To:</label>
@@ -39,6 +39,7 @@ if (!isset($_SESSION['loggedin'])) {
                         <input type="date" name="" id="" class="form-input">
                     </div>
                 </div>
+                <label for="">Report Type</label>
                 <select name="" id="" class="form-input">
                     <option value="">Add new</option>
                     <option value="">View</option>
