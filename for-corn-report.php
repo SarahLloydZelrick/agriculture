cfd2d16acb3b75228e650e27aaddd3d03cc84499
Jake Leogabrel Galindo <jake@rightchoice.email>
2022-11-04 19:50:53 +0800

    rsbsa update

--- for-corn-report.php
+++ for-corn-report.php
@@ -7,7 +7,7 @@ if (!isset($_SESSION['loggedin'])) {
 	header('Location: index.php');
 	exit;
 }
-if($_GET["type"] = "view"){
+if($_GET["type"] == "view"){
     header("Location: corn-report-view.php?dateto=".$_GET['dateto']."&datefrom=".$_GET['datefrom']."");
 	exit;
 }
