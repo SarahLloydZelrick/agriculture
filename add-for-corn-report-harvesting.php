4f3295b406d9b56a02f947f061be995c8ba15b62
Jake Leogabrel Galindo <jake@rightchoice.email>
2022-11-07 16:19:03 +0800

    rsbsa update mond

--- add-for-corn-report-harvesting.php
+++ add-for-corn-report-harvesting.php
@@ -804,13 +804,13 @@ if($_GET["type"] == "view"){
                         $brgy24 = "Sta. Catalina Norte";
                         $brgy25 = "Sta. Catalina Sur";
                         for($i = 1; $i<=25; $i++) {
-                            $brgy[$i] = 'brgy'.$i;
+                            $brgys = 'brgy'.$i;
                             ?>
                             <tr>
                         <td></td>
                         <!-- BARANGAY -->
                         <td>
-                            <input type="text" class="form-input wbrgy" name="barangay<?php echo $i;?>" id="brgy<?php echo $i; ?>" value="<?php echo $brgy[$i]; ?>" readonly>
+                            <input type="text" class="form-input wbrgy" name="barangay<?php echo $i;?>" id="brgy<?php echo $i; ?>" value="<?php echo $brgys; ?>" readonly>
                         </td>
                         <!-- NUMBER OF FARMER -->
                         <td>
