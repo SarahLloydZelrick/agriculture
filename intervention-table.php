d58bd0c4ceb2300eaf3ee07fb9d922bb1f738826
Jake Leogabrel Galindo <jake@rightchoice.email>
2022-11-27 19:38:57 +0800

    rsbsa update mond

--- intervention-table.php
+++ intervention-table.php
@@ -1311,7 +1311,7 @@ if (isset($_POST['btn_release'] )) {
                         . "AND size = '$release_landsize' "
                         . "), $release_programtype)";*/
                 $sqlinserttwo = "INSERT INTO tbl_released (farmerId, name, farmbarangay, crop, size, status, programtype, program) VALUES
-                (SELECT farmerId, name, farmbarangay, crop, size, status FROM tbl_intervention WHERE farmbarangay = '$release_barangay' AND crop = '$release_commodity' AND size = '$release_landsize'), $release_programtype, $release_programs)";
+                ('$release_programtype',(SELECT farmerId, name, farmbarangay, crop, size, status FROM tbl_intervention WHERE farmbarangay = '$release_barangay' AND crop = '$release_commodity' AND size = '$release_landsize'),'$release_programs')";
                 //$sqlinserttwo = "INSERT INTO `tbl_released` (`programtype`,`farmerId`, `name`, `farmbarangay`, `crop`, `size`,`status`,`program`) 
                 //VALUES ('$release_programtype',(SELECT farmerId, name, farmbarangay, crop, size, status FROM tbl_intervention WHERE farmbarangay = '$release_barangay' AND crop = '$release_commodity' AND size = '$release_landsize'),'$release_programs')";
                         if (mysqli_query($con, $sqlinserttwo)) {
